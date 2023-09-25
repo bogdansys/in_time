82f73f1cf678ce105c92efb08bee36b1161e28a1
Bogdan's <bogdanmihai453@gmail.com>
2023-09-26 00:20:13 +0200

    added deadline tracking

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # In Time
 ---
-Streamlined desktop application designed to enhance study motivation by continuously displaying time elapsed in your life, day, week, month, and year. Utilizes a minimalist black-and-white color scheme to minimize distractions and help you focus on your tasks.
+Streamlined desktop application designed to enhance study motivation by continuously displaying time elapsed in your life, day, week, month, year, and also days to your next deadline. Utilizes a minimalist black-and-white color scheme to minimize distractions and help you focus on your tasks.
 
 ## 🌟 Key Features
 ---
-- **Real-time Visualization:** Instantly see your day, week, month, year, and life ticking away in a series of dynamic progress bars. A clear view of the big picture.
+- **Real-time Visualization:** Instantly see your day, week, month, year, life, and days to next deadline ticking away in a series of dynamic progress bars. A clear view of the big picture.
   
-- **Elegant Aesthetics:** A minimalist, sleek black and white theme. A gentle nudge, not a loud alarm, reminding you of life's ephemerality.
+- **Elegant Aesthetics:** A minimalist, sleek black and white theme. A gentle nudge, not a loud alarm, reminding you of life's ephemerality and impending deadlines.
 
-- **Personalized Experience:** On the first launch, you'll be prompted for your birth year. Our app will then tailor the 'Life' bar to your unique journey.
+- **Personalized Experience:** On the first launch, you'll be prompted for your birth year and the number of days to your next deadline. Our app will then tailor the 'Life' and 'Days to d3adl1n3' bars to your unique journey.
   
 - **Optimal Positioning:** By design, the app will nestle in the upper right corner of your screen. Always present, always reminding.
 
 - **Unique Icon:** Our custom icon stands out on your desktop, making it easier to find and harder to forget.
+
+- **Dynamic Deadline Tracker:** After each deadline, you'll be prompted to set a new one. The 'Days to d3adl1n3' progress bar will reset and start counting down again, giving you a visual reminder of the urgency.
 
 ## 🚀 Get Started
 ---
@@ -27,4 +29,10 @@ Streamlined desktop application designed to enhance study motivation by continuo
 
 ---
 
-On your first encounter, our app will inquire about your birth year. This is to personalize your life progress bar. This intimate detail stays with you; we store it locally and won't ask again.
+On your first encounter, our app will inquire about your birth year and the number of days to your next deadline. These details are used to personalize your life progress bar and your deadline progress bar. These intimate details stay with you; we store them locally and won't ask again.
+
+# Troubleshooting
+- If the icon doesnt show correctly, try creating a shortcut of the .exe and using that instead
+- If it doenst look correctly on the first try, that is a Windows issue with transparent GUIs. It can only happen on first start and it will never repeat. Restart the program.
+- Copy & Paste the shortcut in the folder opened by running WinKey + R and "shell:startup". That will now open the proram at windows start.
+
--- time_progress.py
+++ time_progress.py
@@ -8,50 +8,37 @@ from datetime import datetime, timedelta
 class TimeProgressApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.user_age = self.get_user_age()
+        self.user_age = self.get_user_info("user_age.txt", "Enter your age:", QInputDialog.IntInput)
+        self.deadline_days = self.get_user_info("deadline.txt", "Days to next d3adl1n3:", QInputDialog.IntInput)
+        self.deadline_date = datetime.now() + timedelta(days=self.deadline_days)
         self.initUI()
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_progress_bars)
         self.timer.start(1000)
 
     def initUI(self):
-        # Set window attributes for borderless display
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
 
-        # Set the window position to the top right corner of the screen
         screen: QScreen = QApplication.primaryScreen()
         screen_rect: QRect = screen.availableGeometry()
-        self.setGeometry(screen_rect.width() - 210, 30, 200, 200)
+        self.setGeometry(screen_rect.width() - 210, 30, 200, 300)  # Adjusted height
 
         self.setWindowTitle('Time Progress')
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
-        # Set main window background to black
         self.setStyleSheet("background-color: black;")
         
-        self.progress_bars = {
-            'day': QProgressBar(self),
-            'week': QProgressBar(self),
-            'month': QProgressBar(self),
-            'year': QProgressBar(self),
-            'life': QProgressBar(self)
-        }
-
-        for period, bar in self.progress_bars.items():
-            bar.setTextVisible(True)
-            
-            # Set the QLabel text color to white
-            label = QLabel(period.capitalize())
-            label.setStyleSheet("color: white;")
-            
-            # Set the QProgressBar colors
+        self.progress_bars = {}
+        self.days_left_labels = {}
+        
+        for period in ['day', 'week', 'month', 'year', 'life', 'd3adl1n3']:
+            bar = QProgressBar(self)
+            bar.setTextVisible(False)  # Hide default text
             bar.setStyleSheet("""
                 QProgressBar {
                     border: 2px solid white;
-                    text-align: center;
-                    color: white;
                     background-color: black;
                 }
                 QProgressBar::chunk {
@@ -59,16 +46,24 @@ class TimeProgressApp(QWidget):
                 }
             """)
             
+            label = QLabel(period.capitalize())
+            label.setStyleSheet("color: white;")
+            
+            days_left_label = QLabel()
+            days_left_label.setStyleSheet("color: white;")
+            
             self.layout.addWidget(label)
             self.layout.addWidget(bar)
+            self.layout.addWidget(days_left_label)
+            
+            self.progress_bars[period] = bar
+            self.days_left_labels[period] = days_left_label
 
-    def get_user_age(self):
-        # Check if age file exists
-        if os.path.exists("user_age.txt"):
-            with open("user_age.txt", "r") as file:
+    def get_user_info(self, file_name, label_text, input_mode):
+        if os.path.exists(file_name):
+            with open(file_name, "r") as file:
                 return int(file.read().strip())
         else:
-            # If file doesn't exist, ask for age
             dialog = QInputDialog(self)
             dialog.setStyleSheet("""
                 QWidget {
@@ -89,18 +84,18 @@ class TimeProgressApp(QWidget):
                 }
             """)
             dialog.setWindowTitle("Input Dialog")
-            dialog.setLabelText("Enter your age:")
-            dialog.setInputMode(QInputDialog.IntInput)
+            dialog.setLabelText(label_text)
+            dialog.setInputMode(input_mode)
 
             ok = dialog.exec_()
-            age = dialog.intValue()
+            value = dialog.intValue()
 
             if ok:
-                with open("user_age.txt", "w") as file:
-                    file.write(str(age))
-                return age
+                with open(file_name, "w") as file:
+                    file.write(str(value))
+                return value
             else:
-                sys.exit()  # exit if the user cancels the dialog
+                sys.exit()
 
     def calculate_progress(self, period):
         now = datetime.now()
@@ -118,7 +113,10 @@ class TimeProgressApp(QWidget):
             end = start + timedelta(days=365)
         elif period == 'life':
             start = datetime(now.year - self.user_age, 1, 1)
-            end = start + timedelta(days=78*365)  # assuming the average lifespan is 78 years
+            end = start + timedelta(days=78*365)
+        elif period == 'd3adl1n3':
+            start = now
+            end = self.deadline_date
         progress = (now - start) / (end - start)
         return progress
 
@@ -126,6 +124,14 @@ class TimeProgressApp(QWidget):
         for period, bar in self.progress_bars.items():
             progress = self.calculate_progress(period)
             bar.setValue(int(progress * 100))
+            
+            if period == 'd3adl1n3':
+                days_left = max((self.deadline_date - datetime.now()).days, 0)
+                self.days_left_labels[period].setText(f"{days_left} days left")
+
+                if days_left == 0:
+                    new_deadline = self.get_user_info("deadline.txt", "Days to next d3adl1n3:", QInputDialog.IntInput)
+                    self.deadline_date = datetime.now() + timedelta(days=new_deadline)
 
     def run(self):
         self.show()
